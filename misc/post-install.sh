9b25e7469ebcbb1bf8a633c318295154f4c54de1
Erik Nijenhuis <erik@xerdi.com>
2026-07-28 17:53:12 +0200

    Disable libertine installation in post-install script

--- misc/post-install.sh
+++ misc/post-install.sh
@@ -6,7 +6,8 @@ mkdir -p "${TEXMFROOT}/web2c/"
 cp /tmp/texmf.cnf "${TEXMFROOT}/web2c/texmf.cnf"
 mktexlsr
 
-tlmgr install libertine
+# Already included
+#tlmgr install libertine
 
 fc-cache -fv
 luaotfload-tool --update
